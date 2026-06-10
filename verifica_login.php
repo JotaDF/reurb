4f2fbdafee78c0ea4b09eaa230522744fe4dae3f
JotaDF <j.wilson.df@gmail.com>
2026-06-10 14:35:31 -0300

    Update verifica_login.php

--- verifica_login.php
+++ verifica_login.php
@@ -1,11 +1,5 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+ob_start(); 
 session_start();
 date_default_timezone_set('America/Sao_Paulo'); 
 
